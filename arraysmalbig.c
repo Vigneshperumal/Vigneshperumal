fc483cd7ca4a40cb6ee47073f3f0288ef46411c0
Vigneshperumal <vigneshperumal.it13@bitsathy.ac.in>
2016-06-08 09:06:46 +0530

    Update arraysmalbig.c

--- arraysmalbig.c
+++ arraysmalbig.c
@@ -1,26 +1,29 @@
+
 #include<stdio.h>
 int main(){
-  int a[50],size,i,big,small;
+  int a[50],n,i,big,small;
 
   printf("\nEnter the size of the array: ");
-  scanf("%d",&size);
-  printf("\nEnter %d elements in to the array: ", size);
-  for(i=0;i<size;i++)
+  scanf("%d",&n);
+  printf("\nEnter %d elements in to the array: ",n);
+  for(i=0;i<n;i++)
       scanf("%d",&a[i]);
 
-  big=a[0];
-  for(i=1;i<size;i++){
+big=a[0];
+small=a[0];
+  for(i=1;i<n;i++)
+  {
       if(big<a[i])
-           big=a[i];
-  }
-  printf("Largest element: %d",big);
- 
-  small=a[0];
-  for(i=1;i<size;i++){
-      if(small>a[i])
+          { big=a[i];
+           printf("Largest element: %d",big);
+          }
+      else{ 
+          if(small>a[i])
            small=a[i];
+           printf("Smallest element: %d",small);
+           break;
+          }
   }
-  printf("Smallest element: %d",small);
 
   return 0;
 }
